c0d51e1af00770aaef0593b2843967433c522f84
Mikhail Efremov <Mikhail.Efremov@kaspersky.com>
2019-12-10 19:30:12 +0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='strwtime',
+  version='0.0.1',
+  description='Helloworld package',
+  author='Anybody Anybody',
+  author_email='meechanic.design@gmail.com',
+  url='https://github.com/meechanic',
+  license="MIT",
+  scripts=['bin/strwtime_runserver.py'],
+  packages=['strwtime']
+)
