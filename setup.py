0ecf90ec9f56ae3bfc664280343ff04b70b3199b
Mikhail Efremov <meechanic.design@gmail.com>
2019-12-15 15:19:25 +0300

    setup.py changed

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@
 from distutils.core import setup
 
 setup(name='strwtime',
-  version='0.0.2',
-  description='Helloworld package',
-  author='Anybody Anybody',
+  version='0.0.3',
+  description='Strwtime web service',
+  author='Mikhail Efremov',
   author_email='meechanic.design@gmail.com',
   url='https://github.com/meechanic/strwtime',
-  download_url = 'https://github.com/meechanic/strwtime/archive/v0_0_2.tar.gz',
+  download_url = 'https://github.com/meechanic/strwtime/archive/v0_0_3.tar.gz',
   license="MIT",
   scripts=['bin/strwtime_runserver.py'],
   packages=['strwtime']
