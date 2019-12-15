d7ee20deac76b55bdd0cd97e4a1f6cc7ddf43221
Mikhail Efremov <meechanic@gmail.com>
2019-12-15 14:14:38 +0300

    Environment PORT variable recognition added

--- bin/strwtime_runserver.py
+++ bin/strwtime_runserver.py
@@ -2,6 +2,11 @@
 
 import strwtime.core as swtime
 from wsgiref.simple_server import make_server
+import os
 
-httpd = make_server('', 8000, swtime.app)
+if "PORT" in os.environ:
+    port = int(os.environ["PORT"])
+else:
+    port = 8000
+httpd = make_server('', port, swtime.app)
 httpd.serve_forever()
